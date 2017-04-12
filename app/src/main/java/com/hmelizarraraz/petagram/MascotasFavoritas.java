b80fc041d08085317ef7841f05269656b7036f8d
Heriberto Martinez Elizarraraz <hmelizarraraz@hotmail.com>
2017-04-11 21:14:16 -0500

    Se agrega navegacion hacia atras

--- app/src/main/java/com/hmelizarraraz/petagram/MascotasFavoritas.java
+++ app/src/main/java/com/hmelizarraraz/petagram/MascotasFavoritas.java
@@ -2,6 +2,7 @@ package com.hmelizarraraz.petagram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MascotasFavoritas extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MascotasFavoritas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mascotas_favoritas);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.miActionbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
